38aa669af92594bcd9d77e96b22830124bdfb10f
andretipolt <andretipoltlopes@gmail.com>
2024-05-17 19:17:19 -0300

    Criando camada de controladores

---
+++ Back_End_Spring/app/src/main/java/app/concessionaria/app/controllers/AutomovelController.java
@@ -0,0 +1,10 @@
+package app.concessionaria.app.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/automoveis")
+public class AutomovelController {
+    
+}
