8ce327e160bae92101710b414f83b06a500c6248
andretipolt <andretipoltlopes@gmail.com>
2024-05-21 20:18:57 -0300

    Ajustando automoveis

---
+++ Front_End_Angular/app/src/app/automoveis/automoveis.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { AutomoveisRoutingModule } from './automoveis-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    AutomoveisRoutingModule
+  ]
+})
+export class AutomoveisModule { }
