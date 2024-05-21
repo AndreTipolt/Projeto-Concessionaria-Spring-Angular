a14663f72d024cd7f12b3658ca5492af9de7d8d5
andretipolt <andretipoltlopes@gmail.com>
2024-05-21 20:00:25 -0300

    Automovel Controller

--- Back_End_Spring/app/src/main/java/app/concessionaria/app/controllers/AutomovelController.java
+++ Back_End_Spring/app/src/main/java/app/concessionaria/app/controllers/AutomovelController.java
@@ -3,13 +3,20 @@ package app.concessionaria.app.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import app.concessionaria.app.dtos.req.AutomovelDTOReq;
 import app.concessionaria.app.dtos.res.AutomovelDTORes;
 import app.concessionaria.app.services.AutomovelService;
+import jakarta.validation.Valid;
 
 
 @RestController
@@ -19,13 +26,36 @@ public class AutomovelController {
     @Autowired
     private AutomovelService automovelService;
 
-
     @GetMapping("findAll")
-    public ResponseEntity<List<AutomovelDTORes>> findAllAutomoveis() {
+    public ResponseEntity<List<AutomovelDTORes>> findAllAutomovels() {
         
-        List<AutomovelDTORes> listAutomoveisDTO = this.automovelService.findAllAutomoveis();
+        List<AutomovelDTORes> listClientDTORes = this.automovelService.findAllAutomoveis();
 
-        return ResponseEntity.ok().body(listAutomoveisDTO);
+        return ResponseEntity.ok().body(listClientDTORes);
+    }
+
+    @PostMapping("save")
+    public ResponseEntity<Void> saveAutomovel(@Valid @RequestBody AutomovelDTOReq automovelDTOReq) {
+        
+        this.automovelService.saveAutomovel(automovelDTOReq);
+
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
+    @GetMapping("/{idAutomovel}")
+    public ResponseEntity<AutomovelDTORes> findAutomovelById(@PathVariable String idAutomovel) {
+        
+        AutomovelDTORes automovelDTO = this.automovelService.findAutomovelById(idAutomovel);
+
+        return ResponseEntity.ok().body(automovelDTO);
     }
     
+
+    @DeleteMapping(value = "/{idAutomovel}")
+    public ResponseEntity<Void> deleteAutomovelById(@PathVariable String idAutomovel) {
+
+        this.deleteAutomovelById(idAutomovel);
+
+        return ResponseEntity.ok().build();
+    }
 }
